777c7dadf3e7f4f11851710295918dc96e6fbf82
ahogga11 <122407188+ahogga11@users.noreply.github.com>
2023-01-25 14:11:46 -0700

    LabPractice
    1/25/23- Coding practice

---
+++ LabPractice.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string name = "Alissa";
+      Console.WriteLine(name); 
+      int number = 21;
+      Console.WriteLine(number);
+     }
+  }
+}
+
