57826803081d80a368f62b2b325e14acc7de0a25
ahogga11 <122407188+ahogga11@users.noreply.github.com>
2023-02-01 20:14:57 -0700

    ArraysForeachLoops
    2/1/23- ArraysForeachLoops

---
+++ Basics/Unit1c/ArraysForeachLoops.cs
@@ -0,0 +1,11 @@
+using System;
+					
+public class Program
+{
+	public void Main()
+	{
+	string[] cars = {"Mustang", "Camero", "HellCat", "Corvette", "Viper", "McLaren"};
+		foreach(var car in cars) {
+			Console.WriteLine("I love my " + car + ".");		}
+	}
+}
