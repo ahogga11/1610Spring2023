226b25aa066f1e0f6fd7d31d40563d75f1db10ee
ahogga11 <122407188+ahogga11@users.noreply.github.com>
2023-02-08 20:55:57 -0700

    ForeachLoops
    2/8/23- "Try it yourself" practice

---
+++ Basics/Unit1c/ForeachLoops.cs
@@ -0,0 +1,13 @@
+using System;
+					
+public class Program
+{
+	public void Main()
+	{
+	string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+foreach (string i in cars) 
+{
+  Console.WriteLine(i);
+		}
+	}
+}
