98de6b7463d4f1b0c692391ab87bafb3a16ad9b7
ahogga11 <122407188+ahogga11@users.noreply.github.com>
2023-02-08 21:05:39 -0700

    ArrayLoops
    2/8/23- "Try it yourself" practice

---
+++ Basics/Unit1c/ArrayLoops.cs
@@ -0,0 +1,13 @@
+using System;
+					
+public class Program
+{
+	public void Main()
+	{
+	string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+for (int i = 0; i < cars.Length; i++) 
+{
+  Console.WriteLine(cars[i]);
+		}
+	}
+}
