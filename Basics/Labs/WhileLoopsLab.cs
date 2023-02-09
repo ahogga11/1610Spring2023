eb835f81af37bc729187142bfc0d715fe3f24b99
ahogga11 <122407188+ahogga11@users.noreply.github.com>
2023-02-08 20:55:04 -0700

    WhileLoopsLab
    2/8/23- Changed code

--- Basics/Labs/WhileLoopsLab.cs
+++ Basics/Labs/WhileLoopsLab.cs
@@ -4,10 +4,10 @@ public class Program
 {
 	public static void Main()
 	{
-		string[] playerTypes = {"Pawn", "Pawn", "Pawn"};
+		string[] playerTypes = {"King", "Soldier", "Poet"};
 		
 		for (var i = 0; i < playerTypes.Length ; i++) {
-			playerTypes[i] = "Queen";
+			playerTypes[i] = "Jester";
 		}
 		
 		foreach(var playerType in playerTypes) {
