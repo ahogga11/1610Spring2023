45b632bec958d253caa5bf2bf3ce08570256ae04
ahogga11 <122407188+ahogga11@users.noreply.github.com>
2023-02-15 23:13:19 -0700

    Update MainPlayer.cs
    2/15/23- Practice

--- SpringClass/Assets/MainPlayer.cs
+++ SpringClass/Assets/MainPlayer.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 using System.Collections;
 
-public class MainPlayer : MonoBehaviour
+public class MainPlayer : MonoBehaviour 
 {
-
-    void Start()
+    public string myName;
+    
+    void Start () 
     {
-        Debug.Log("I am alive!");
+        Debug.Log("I am alive and my name is " + myName);
     }
 }
