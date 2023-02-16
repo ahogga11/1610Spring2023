f0fab59f798c74e58bb72694d2518fc7cf29505f
ahogga11 <122407188+ahogga11@users.noreply.github.com>
2023-02-15 23:08:56 -0700

    Update MainPlayer.cs
    2/15/23- Practice

--- SpringClass/Assets/MainPlayer.cs
+++ SpringClass/Assets/MainPlayer.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
-System.Collections;
+using System.Collections;
 
 public class MainPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
         Debug.Log("I am alive!");
