bedd1aa0b0344bff7ae5b560d62c474f6836747b
ahogga11 <122407188+ahogga11@users.noreply.github.com>
2023-02-23 17:25:27 -0700

    Update
    2/23/23

--- SpringClass/Assets/Inventory.cs
+++ SpringClass/Assets/Inventory.cs
@@ -1,45 +1,44 @@
 using UnityEngine;
-using System.Collections;
 
 public class Inventory : MonoBehaviour
 {
     public class Stuff
     {
-        public int bullets;
-        public int grenades;
-        public int rockets;
-        public float fuel;
+        public int Bullets;
+        public int Grenades;
+        public int Rockets;
+        public float Fuel;
         
         public Stuff(int bul, int gre, int roc)
         {
-            bullets = bul;
-            grenades = gre;
-            rockets = roc;
+            Bullets = bul;
+            Grenades = gre;
+            Rockets = roc;
         }
         
         public Stuff(int bul, float fu)
         {
-            bullets = bul;
-            fuel = fu;
+            Bullets = bul;
+            Fuel = fu;
         }
         
         // Constructor
         public Stuff ()
         {
-            bullets = 1;
-            grenades = 1;
-            rockets = 1;
+            Bullets = 1;
+            Grenades = 1;
+            Rockets = 1;
         }
     }
     
 
     // Creating an Instance (an Object) of the Stuff class
-    public Stuff myStuff = new Stuff(50, 5, 5);
+    public Stuff MyStuff = new Stuff(50, 5, 5);
     
-    public Stuff myOtherStuff = new Stuff(50, 1.5f);
+    public Stuff MyOtherStuff = new Stuff(50, 1.5f);
     
     void Start()
     {
-        Debug.Log(myStuff.bullets); 
+        Debug.Log(MyStuff.Bullets); 
     }
 }
