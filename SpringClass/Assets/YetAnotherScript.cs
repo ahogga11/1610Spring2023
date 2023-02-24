68705a7bc1649a26f739ec190ad553b41f2a63e9
ahogga11 <122407188+ahogga11@users.noreply.github.com>
2023-02-23 20:24:46 -0700

    Update
    2/23/23

--- SpringClass/Assets/YetAnotherScript.cs
+++ SpringClass/Assets/YetAnotherScript.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
+using System.Collections;
 
-public partial class YetAnotherScript : MonoBehaviour
+public class YetAnotherScript : MonoBehaviour
 {
     public int numberOfPlayerDeaths = 3;
 }
