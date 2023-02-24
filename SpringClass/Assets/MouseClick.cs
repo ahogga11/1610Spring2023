04e4358edc214070f75f094f6f4799f9109102e8
ahogga11 <122407188+ahogga11@users.noreply.github.com>
2023-02-23 17:33:53 -0700

    Update
    2/23/23

--- SpringClass/Assets/MouseClick.cs
+++ SpringClass/Assets/MouseClick.cs
@@ -1,19 +1,18 @@
 using UnityEngine;
-using System.Collections;
 
 public class MouseClick : MonoBehaviour
 {
 
-    private Rigidbody rb;
+    private Rigidbody _rb;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>();
     }
 
     void OnMouseDown ()
     {
-        rb.AddForce(-transform.forward * 500f);
-        rb.useGravity = true;
+        _rb.AddForce(-transform.forward * 500f);
+        _rb.useGravity = true;
     }
 }
