8f4930fb49ccb1a4ef0f1dfa55e5c11a6733f32c
Alissa <11025469@uvu.edu>
2023-04-13 21:32:30 -0600

    UFO

--- UFO Defense Force/Assets/Scripts/PlayerController.cs
+++ UFO Defense Force/Assets/Scripts/PlayerController.cs
@@ -1,26 +1,26 @@
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
 
-    public float speed;
+    public float speed = 25;
 
-    public float xRange;
+    public float xRange = 30;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-    
+    public Transform blaster;
+
+    public GameObject lazerBolt;
+
+
     // Update is called once per frame
     void Update()
     {
-        // Set horizontalInput to Receive Values from Keyboard
+        // Set horizontalInput to receive values from keyboard
         horizontalInput = Input.GetAxis("Horizontal");
-        
-        //Moves Player left and right
+
+        // Moves Player left and right
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
         // Keep player within bounds 
         // Left Side Wall
@@ -29,9 +29,21 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
         // Right Side Wall
-            if (transform.position.x < xRange)
-            {
-                transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-            }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        // If space bar is pressed, fire lazerBolt
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+        // Create lazerBolt at the blaster transform position maintaining the object's rotation
+            Instantiate(lazerBolt, blaster.transform.position, lazerBolt.transform.rotation);
         }
     }
+    // Delete any object with a trigger that hits the player
+
+        private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
