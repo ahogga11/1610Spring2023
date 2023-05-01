4cbd1d990227d630dd3d97a0a17be55e6c6d3952
ahogga11 <122407188+ahogga11@users.noreply.github.com>
2023-04-30 19:57:14 -0600

    Create

---
+++ SpringClass/Challenges/UFO Defense Force/Scripts/EnemySpawnManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnManager : MonoBehaviour
+{
+    public GameObject[] ufoPrefabs; // Array to store UFO ships
+
+    private float spawnRangeX = 20f;
+
+    private float spawnPosZ = 20f;
+
+    private float startDelay = 2f;
+
+    private float spawnInterval = 1.5f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomUFO", startDelay, spawnInterval);
+    }
+
+    void SpawnRandomUFO()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX,spawnRangeX),0,spawnPosZ);
+        int ufoIndex = Random.Range(0,ufoPrefabs.Length);
+        Instantiate(ufoPrefabs[ufoIndex],spawnPos, ufoPrefabs[ufoIndex].transform.rotation);
+    }
+}
