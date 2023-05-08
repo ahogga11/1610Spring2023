2f7b0a9d5dcdec4cb3652e8923748ac4de8221c3
ahogga11 <122407188+ahogga11@users.noreply.github.com>
2023-05-08 00:05:19 -0600

    Create

---
+++ SpringClass/Challenges/Unit 6 Challenge/Assets/Scripts/SpawnCoinScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SpawnCoinScript : MonoBehaviour
+{
+    public GameObject coinPrefab;
+
+    public int coin;
+
+    public TextMeshProUGUI textCoin;
+
+    public float timeRemaining = 2f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        if (timeRemaining < 0)
+        {
+            timeRemaining = Random.Range(1, 3);
+            spawn_coin();
+        }
+    }
+    public void spawn_coin()
+        {
+            Vector3 randomPosition = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+            Instantiate(coinPrefab, randomPosition, Quaternion.identity);
+        }
+    public void OnTriggerEnter(Collider Col)
+    {
+        if (Col.gameObject.tag == "Coin")
+        {
+            Debug.Log("Coin Collected!");
+            coin = coin + 1;
+            textCoin.text = coin.ToString();
+            Col.gameObject.SetActive(false);
+            Destroy(Col);
+            Debug.Log("Coin Collected!");
+        }
+
+        
+    }
+}
